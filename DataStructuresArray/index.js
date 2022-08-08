e275877e2b90718ad1038f7a5119b11cfe36047a
Dani Schuhman <dani8439@aol.com>
2022-08-08 16:05:02 -0400

    Solution: Merged Sorted Arrays.

--- DataStructuresArray/index.js
+++ DataStructuresArray/index.js
@@ -135,3 +135,37 @@ const reverse4 = str => [...str].reverse().join('');
 function mergeSortedArrays(arr1, arr2) {
     return arr1.concat(arr2).sort();
 }
+
+//////////////
+// Merged Sort Solution
+
+function mergeSortedArrays(arr1, arr2) {
+    const mergedArray = [];
+    let array1Item = arr1[0];
+    let array2Item = arr2[0];
+    let i = 1;
+    let j = 1;
+
+    // Check input 
+    if (arr1.length === 0) {
+        return arr2;
+    }
+    if (arr2.length === 0) {
+        return arr1;
+    }
+
+    while (array1Item || array2Item) {
+        console.log(array1Item, array2Item);
+        if (!array2Item || array1Item < array2Item) {
+            mergedArray.push(array1Item)
+            array1Item = arr1[i];
+            i++
+        } else {
+            mergedArray.push(array2Item);
+            array2Item = arr2[j];
+            j++;
+        }
+    }
+
+    return mergedArray;
+}
--- DataStructuresArray/notes.md
+++ DataStructuresArray/notes.md
@@ -193,3 +193,38 @@ const reverse4 = str => [...str].reverse().join('');
 mergeSortedArrays([0,3,4,31], [4,5,30]);
 // [0, 3, 4, 4, 6, 30, 31]
 ```
+
+# Solution: Merge Sorted Arrays 
+
+```js
+function mergeSortedArrays(arr1, arr2) {
+    const mergedArray = [];
+    let array1Item = arr1[0];
+    let array2Item = arr2[0];
+    let i = 1;
+    let j = 1;
+
+    // Check input 
+    if (arr1.length === 0) {
+        return arr2;
+    }
+    if (arr2.length === 0) {
+        return arr1;
+    }
+
+    while (array1Item || array2Item) {
+        console.log(array1Item, array2Item);
+        if (!array2Item || array1Item < array2Item) {
+            mergedArray.push(array1Item)
+            array1Item = arr1[i];
+            i++
+        } else {
+            mergedArray.push(array2Item);
+            array2Item = arr2[j];
+            j++;
+        }
+    }
+
+    return mergedArray;
+}
+```
