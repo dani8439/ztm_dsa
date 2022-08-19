ff0c702215a536507ce037e11eace54e96656bf6
Dani Schuhman <dani8439@aol.com>
2022-08-19 15:20:40 -0400

    exercise setup.

--- LinkedLists/index.js
+++ LinkedLists/index.js
@@ -54,7 +54,12 @@ class LinkedList {
         this.tail = this.head;
         this.length = 1;
     }
+    append(value) {
+        // Code here
+    }
 }
 
 const myLinkedList = new LinkedList(10)
+myLinkedList.append(5);
+myLinkedList.append(16);
 console.log(myLinkedList)
